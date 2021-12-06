2fb63330163a05120dea662a6a2541f7f99d3560
w8f <haya22aws@gmail.com>
2021-12-07 08:52:59 +0900

    認証周りの実装

--- back/src/index.js
+++ back/src/index.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require("@prisma/client");
 const { ApolloServer } = require("apollo-server");
 const fs = require("fs");
 const path = require("path");
-
+const { getUserId } = require("./utils");
 const prisma = new PrismaClient();
 
 // 1
@@ -14,52 +14,61 @@ let links = [
   },
 ];
 
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+
 // resolversオブジェクトは、GraphQLスキーマの実際の実装です。
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    // nullを返すとエラーを返す（String! と定義しているため。）
-    // info: () => null,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany();
-    },
-    link: (parent, args) => {
-      return links.find((link) => link.id == args.id);
-    },
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      // let idCount = links.length;
-      // const link = {
-      //   id: `link-${idCount++}`,
-      //   url: args.url,
-      //   description: args.description,
-      // };
-      // links.push(link);
-      // return link;
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      });
-      return newLink;
-    },
-    updateLink: (parent, args) => {
-      links = links
-        .filter((link) => link.id == args.id)
-        .map((link) => ({
-          id: link.id,
-          url: args.url,
-          description: args.description,
-        }));
-      return links.find((link) => link.id == args.id);
-    },
-    deleteLink: (parent, args) => {
-      links = links.filter((link) => link.id !== args.id);
-      return null;
-    },
-  },
+  Query,
+  Mutation,
+  User,
+  Link,
+  // Query: {
+  //   info: () => `This is the API of a Hackernews Clone`,
+  //   // nullを返すとエラーを返す（String! と定義しているため。）
+  //   // info: () => null,
+  //   feed: async (parent, args, context) => {
+  //     return context.prisma.link.findMany();
+  //   },
+  //   link: (parent, args) => {
+  //     return links.find((link) => link.id == args.id);
+  //   },
+  // },
+  // Mutation: {
+  //   post: (parent, args, context, info) => {
+  //     // let idCount = links.length;
+  //     // const link = {
+  //     //   id: `link-${idCount++}`,
+  //     //   url: args.url,
+  //     //   description: args.description,
+  //     // };
+  //     // links.push(link);
+  //     // return link;
+  //     const newLink = context.prisma.link.create({
+  //       data: {
+  //         url: args.url,
+  //         description: args.description,
+  //       },
+  //     });
+  //     return newLink;
+  //   },
+  //   updateLink: (parent, args) => {
+  //     links = links
+  //       .filter((link) => link.id == args.id)
+  //       .map((link) => ({
+  //         id: link.id,
+  //         url: args.url,
+  //         description: args.description,
+  //       }));
+  //     return links.find((link) => link.id == args.id);
+  //   },
+  //   deleteLink: (parent, args) => {
+  //     links = links.filter((link) => link.id !== args.id);
+  //     return null;
+  //   },
+  // },
 };
 
 // どのような API 操作を受け入れ、どのように解決すべきかをサーバに伝えます。
@@ -68,8 +77,17 @@ const server = new ApolloServer({
   resolvers,
   // PrismaClientのインスタンスを（prismaとして）アタッチしているので、
   // すべてのリゾルバでcontext.prismaにアクセスできるようになっています。
-  context: {
-    prisma,
+
+  // コンテキストを返す関数として、
+  // 入力されたGraphQLクエリ（またはミューテーション）を伝えるHTTPリクエストをコンテキストに添付できる。
+  // これにより、リゾルバはAuthorizationヘッダを読み取り、
+  // リクエストを送信したユーザーが要求された操作を実行する資格があるかどうかを検証することができます。
+  context: ({ req }) => {
+    return {
+      ...req,
+      prisma,
+      userId: req && req.headers.authorization ? getUserId(req) : null,
+    };
   },
 });
 
