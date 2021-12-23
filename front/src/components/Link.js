af46ce70b3bf6a2c4544658864e87f52b0cba777
w8f <haya22aws@gmail.com>
2021-12-23 21:55:29 +0900

    vote mutationのcache管理の実装

--- front/src/components/Link.js
+++ front/src/components/Link.js
@@ -1,6 +1,7 @@
 import { LINKS_PER_PAGE, AUTH_TOKEN } from "../constants";
 import { timeDifferenceForDate } from "../utils";
 import { useMutation, gql } from "@apollo/client";
+import { FEED_QUERY } from "./LinkList";
 
 const Link = (props) => {
   const { link } = props;
@@ -33,6 +34,33 @@ const Link = (props) => {
   const [vote] = useMutation(VOTE_MUTATION, {
     variables: {
       linkId: link.id,
+    },
+    // キャッシュ管理
+    // update callbackを追加
+    // mutation実行後に実行される
+    update(cache, { data: { vote } }) {
+      const { feed } = cache.readQuery({
+        query: FEED_QUERY,
+      });
+
+      const updateLinks = feed.links.map((feedLink) => {
+        if (feedLink.id === link.id) {
+          return {
+            ...feedLink,
+            votes: [...feedLink.votes, vote],
+          };
+        }
+        return feedLink;
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            links: updateLinks,
+          },
+        },
+      });
     },
   });
 
