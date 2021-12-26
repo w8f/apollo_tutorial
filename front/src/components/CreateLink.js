23987b5cdd9d3d7541e2e43e5b2206139b892c33
w8f <haya22aws@gmail.com>
2021-12-26 15:26:34 +0900

    createLink処理後のキャッシュ管理実装

--- front/src/components/CreateLink.js
+++ front/src/components/CreateLink.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
+import { LINKS_PER_PAGE } from "../constants";
+import { FEED_QUERY } from "./LinkList";
 
 const CREATE_LINK_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -24,6 +26,36 @@ const CreateLink = () => {
     variables: {
       description: formState.description,
       url: formState.url,
+    },
+
+    update: (cache, { data: { post } }) => {
+      const take = LINKS_PER_PAGE;
+      const skip = 0;
+      const orderBy = { createdAt: "desc" };
+
+      const data = cache.readQuery({
+        query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
+
+      cache.writeQuery({
+        query: FEED_QUERY,
+        data: {
+          feed: {
+            id: post.id,
+            links: [post, ...data.feed.links],
+          },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
+      });
     },
     onCompleted: () => navigate("/"),
   });
